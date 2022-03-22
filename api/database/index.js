c73bcfb3288921a708bf3d2c6554b919a30d1f3d
sinariac <sinariac@gmail.com>
2022-03-22 00:17:41 -0300

    Banco configurado

--- api/database/index.js
+++ api/database/index.js
@@ -1,4 +1,9 @@
 const Sequelize = require("sequelize");
 const config = require("../config/config");
+
+const SalaCinema = require("../models/SalaCinema")
 const connection = new Sequelize(config);
+
+SalaCinema.init(connection);
+
 module.exports = connection
